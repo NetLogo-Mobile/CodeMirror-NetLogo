4f0efacae0e421bb416bde3c7b2f548ca503a376
wubbalubbadu <wavgoddbye@gmail.com>
2022-10-12 17:46:29 -0500

    apis w/o highlight, find, replace

--- src/lang/lang.js
+++ src/lang/lang.js
@@ -3,8 +3,8 @@ import {LRParser} from "@lezer/lr"
 import {keyword} from "./tokenizer.js"
 export const parser = LRParser.deserialize({
   version: 14,
-  states: "!WQYQROOO!QQRO'#CmOOQQ'#Cr'#CrOOQQ'#Cn'#CnQYQROOOOQQ,59X,59XO!XQRO,59XOOQQ-E6l-E6lOOQQ1G.s1G.s",
-  stateData: "!`~OeOSZOS~OQQORQOSQOTQOUQOVQOWQOXQOYQO]QO^QO`PO~O_TO~PYO_WO~PYO",
+  states: "!WQYQROOO!TQRO'#CmOOQQ'#Cr'#CrOOQQ'#Cn'#CnQYQROOOOQQ,59X,59XO![QRO,59XOOQQ-E6l-E6lOOQQ1G.s1G.s",
+  stateData: "!c~OeOSZOS~OPQOQQORQOSQOTQOUQOVQOWQOXQOYQO]QO^QO`PO~O_TO~PYO_WO~PYO",
   goto: "!OgPPPPPPPPPPPPPPPPPhnPPPxXQOPSUQSOQUPTVSUXROPSU",
   nodeNames: "⚠ Identifier Directives Commands Extensions Reporters TurtleVars PatchVars LinkVars Constants Unsupported LineComment Program Numeric String ] [ Application",
   maxTerm: 22,
@@ -12,7 +12,7 @@ export const parser = LRParser.deserialize({
     ["openedBy", 15,"["],
     ["closedBy", 16,"]"]
   ],
-  skippedNodes: [0,1,11],
+  skippedNodes: [0,11],
   repeatNodeCount: 1,
   tokenData: "#h~RXXYnYZn]^npqnrs!P!Q![!n!]!^#R!}#O#^#P#Q#c~sSe~XYnYZn]^npqn~!STOr!Prs!cs#O!P#O#P!h#P~!P~!hO^~~!kPO~!P~!sQ]~!O!P!y!Q![!n~#OP]~!Q![!y~#WQZ~OY#RZ~#R~#cO`~~#hO_~",
   tokenizers: [0, keyword],
