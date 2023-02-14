9db8037c9d7c96d6f1078b4dc4aa4b04e505bbe4
rbagley <rbagley@users.noreply.github.com>
2023-02-14 20:02:22 +0000

    deploy: 0da8565e0e1b18f08e6608f2500fb1f3fc839523

--- editor.bundle.js
+++ editor.bundle.js
@@ -29098,89 +29098,129 @@ if(!String.prototype.matchAll) {
    ];
 
    const prettify = function (view) {
-       let changes = [];
+       let from = view.state.selection.main.from;
+       let to = view.state.selection.main.to;
+       let doc = view.state.doc.toString().substring(from, to);
+       //eliminate extra spacing
+       let new_doc = initialSpaceRemoval(doc);
+       view.dispatch(view.state.replaceSelection(new_doc));
+       view.dispatch({ selection: { anchor: from, head: from + new_doc.length } });
+       //add in new lines based on grammar
+       view.dispatch({
+           changes: addSpacing(view, view.state.selection.main.from, view.state.selection.main.to),
+       });
+       //ensure spacing is correct
+       from = view.state.selection.main.from;
+       to = view.state.selection.main.to;
+       new_doc = finalSpacing(view.state.doc.toString().substring(from, to));
+       view.dispatch(view.state.replaceSelection(new_doc));
+       view.dispatch({ selection: { anchor: from, head: from + new_doc.length } });
+       //add indentation
+       view.dispatch({
+           changes: indentRange(view.state, view.state.selection.main.from, view.state.selection.main.to),
+       });
+   };
+   const prettifyAll = function (view) {
        let doc = view.state.doc.toString();
        //eliminate extra spacing
+       let new_doc = initialSpaceRemoval(doc);
+       view.dispatch({ changes: { from: 0, to: doc.length, insert: new_doc } });
+       //give certain nodes their own lines
+       view.dispatch({
+           changes: addSpacing(view, 0, view.state.doc.toString().length),
+       });
+       //ensure spacing is correct
+       doc = view.state.doc.toString();
+       new_doc = finalSpacing(doc);
+       view.dispatch({ changes: { from: 0, to: doc.length, insert: new_doc } });
+       //add indentation
+       view.dispatch({
+           changes: indentRange(view.state, 0, view.state.doc.toString().length),
+       });
+   };
+   const initialSpaceRemoval = function (doc) {
        let new_doc = doc.replace(/\n\s+/g, '\n');
        new_doc = new_doc.replace(/(\n[^;\n]+)(\n\s*\[)/g, '$1 [');
        new_doc = new_doc.replace(/(\n[^;\n]+)(\n\s*\])/g, '$1 ]');
        new_doc = new_doc.replace(/(\[\n\s*)([\w\(])/g, '[ $2');
        new_doc = new_doc.replace(/(\[|\]|\(|\))/g, ' $1 ');
        new_doc = new_doc.replace(/ +/g, ' ');
-       view.dispatch({ changes: { from: 0, to: doc.length, insert: new_doc } });
-       doc = view.state.doc.toString();
+       return new_doc;
+   };
+   const finalSpacing = function (doc) {
+       let new_doc = doc.replace(/\n\s+/g, '\n');
+       new_doc = new_doc.replace(/\s+\n/g, '\n');
+       new_doc = new_doc.replace(/(\nto[ -])/g, '\n$1');
+       new_doc = new_doc.replace(/(\n[\w-]+-own)/g, '\n$1');
+       return new_doc;
+   };
+   const addSpacing = function (view, from, to) {
+       let changes = [];
+       let doc = view.state.doc.toString();
        //give certain nodes their own lines
        syntaxTree(view.state)
            .cursor()
            .iterate((node) => {
            var _a, _b;
-           if (((((_a = node.node.parent) === null || _a === void 0 ? void 0 : _a.name) == 'Program' && node.name != 'LineComment') ||
-               node.name == 'To' ||
-               node.name == 'End' ||
-               (node.name == 'ProcedureContent' &&
-                   ((_b = node.node.parent) === null || _b === void 0 ? void 0 : _b.name) != 'CodeBlock')) &&
-               node.from > 0) {
-               changes.push({ from: node.from, to: node.from, insert: '\n' });
-           }
-           else if (node.name == 'CodeBlock' &&
-               checkBlock(node.node, 'ProcedureContent', doc)) {
-               node.node.getChildren('ProcedureContent').map((child) => {
-                   changes.push({ from: child.from, to: child.from, insert: '\n' });
-               });
-               node.node.getChildren('CloseBracket').map((child) => {
-                   changes.push({ from: child.from, to: child.from, insert: '\n' });
-               });
-           }
-           else if (node.name == 'ReporterBlock' &&
-               checkBlock(node.node, 'ReporterContent', doc)) {
-               node.node.getChildren('ReporterContent').map((child) => {
-                   changes.push({ from: child.from, to: child.from, insert: '\n' });
-               });
-               node.node.getChildren('CloseBracket').map((child) => {
-                   changes.push({ from: child.from, to: child.from, insert: '\n' });
-               });
-           }
-           else if (node.name == 'AnonymousProcedure' &&
-               (checkBlock(node.node, 'ReporterContent', doc) ||
-                   checkBlock(node.node, 'ProcedureContent', doc))) {
-               console.log(changes.length);
-               node.node.getChildren('ProcedureContent').map((child) => {
-                   changes.push({ from: child.from, to: child.from, insert: '\n' });
-               });
-               node.node.getChildren('ReporterContent').map((child) => {
-                   changes.push({ from: child.from, to: child.from, insert: '\n' });
-               });
-               node.node.getChildren('CloseBracket').map((child) => {
-                   changes.push({ from: child.from, to: child.from, insert: '\n' });
-               });
-               console.log(changes.length);
-           }
-           if (['Extensions', 'Globals', 'BreedsOwn'].includes(node.name)) {
-               if (doc.substring(node.from, node.to).includes('\n')) {
+           if (node.from >= from && node.to <= to) {
+               if (((((_a = node.node.parent) === null || _a === void 0 ? void 0 : _a.name) == 'Program' &&
+                   node.name != 'LineComment') ||
+                   node.name == 'To' ||
+                   node.name == 'End' ||
+                   (node.name == 'ProcedureContent' &&
+                       ((_b = node.node.parent) === null || _b === void 0 ? void 0 : _b.name) != 'CodeBlock')) &&
+                   node.from > 0) {
+                   changes.push({ from: node.from, to: node.from, insert: '\n' });
+               }
+               else if (node.name == 'CodeBlock' &&
+                   checkBlock(node.node, 'ProcedureContent', doc)) {
+                   node.node.getChildren('ProcedureContent').map((child) => {
+                       changes.push({ from: child.from, to: child.from, insert: '\n' });
+                   });
                    node.node.getChildren('CloseBracket').map((child) => {
                        changes.push({ from: child.from, to: child.from, insert: '\n' });
                    });
-                   node.node.getChildren('Extension').map((child) => {
+               }
+               else if (node.name == 'ReporterBlock' &&
+                   checkBlock(node.node, 'ReporterContent', doc)) {
+                   node.node.getChildren('ReporterContent').map((child) => {
                        changes.push({ from: child.from, to: child.from, insert: '\n' });
                    });
-                   node.node.getChildren('Identifier').map((child) => {
+                   node.node.getChildren('CloseBracket').map((child) => {
                        changes.push({ from: child.from, to: child.from, insert: '\n' });
                    });
                }
+               else if (node.name == 'AnonymousProcedure' &&
+                   (checkBlock(node.node, 'ReporterContent', doc) ||
+                       checkBlock(node.node, 'ProcedureContent', doc))) {
+                   console.log(changes.length);
+                   node.node.getChildren('ProcedureContent').map((child) => {
+                       changes.push({ from: child.from, to: child.from, insert: '\n' });
+                   });
+                   node.node.getChildren('ReporterContent').map((child) => {
+                       changes.push({ from: child.from, to: child.from, insert: '\n' });
+                   });
+                   node.node.getChildren('CloseBracket').map((child) => {
+                       changes.push({ from: child.from, to: child.from, insert: '\n' });
+                   });
+                   console.log(changes.length);
+               }
+               if (['Extensions', 'Globals', 'BreedsOwn'].includes(node.name)) {
+                   if (doc.substring(node.from, node.to).includes('\n')) {
+                       node.node.getChildren('CloseBracket').map((child) => {
+                           changes.push({ from: child.from, to: child.from, insert: '\n' });
+                       });
+                       node.node.getChildren('Extension').map((child) => {
+                           changes.push({ from: child.from, to: child.from, insert: '\n' });
+                       });
+                       node.node.getChildren('Identifier').map((child) => {
+                           changes.push({ from: child.from, to: child.from, insert: '\n' });
+                       });
+                   }
+               }
            }
        });
-       view.dispatch({ changes: changes });
-       //ensure spacing is correct
-       doc = view.state.doc.toString();
-       new_doc = doc.replace(/\n\s+/g, '\n');
-       new_doc = doc.replace(/\s+\n/g, '\n');
-       new_doc = new_doc.replace(/(\nto[ -])/g, '\n$1');
-       new_doc = new_doc.replace(/(\n[\w-]+-own)/g, '\n$1');
-       view.dispatch({ changes: { from: 0, to: doc.length, insert: new_doc } });
-       //add indentation
-       view.dispatch({
-           changes: indentRange(view.state, 0, view.state.doc.toString().length),
-       });
+       return changes;
    };
    //checks if code block needs to be multiline
    const checkBlock = function (node, childName, doc) {
@@ -29351,9 +29391,13 @@ if(!String.prototype.matchAll) {
        Focus() {
            this.CodeMirror.focus();
        }
-       /** Prettify: Prettify the NetLogo code. */
+       /** Prettify: Prettify the selection ofNetLogo code. */
        Prettify() {
            prettify(this.CodeMirror);
+       }
+       /** PrettifyAll: Prettify all the NetLogo code. */
+       PrettifyAll() {
+           prettifyAll(this.CodeMirror);
        }
        /** CloseCompletion: Forcible close the auto completion. */
        CloseCompletion() {
