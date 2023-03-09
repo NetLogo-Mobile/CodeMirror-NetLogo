7af6b8d05c729def4d0d2972c32c6ab5d196bbac
John Chen <john@soobb.com>
2023-03-09 16:55:03 -0600

    Force parsing of the full file

--- src/editor.ts
+++ src/editor.ts
@@ -297,7 +297,7 @@ export class GalapagosEditor {
 
   /** ForceParse: Force the editor to finish any parsing. */
   ForceParse() {
-    forceParsing(this.CodeMirror);
+    forceParsing(this.CodeMirror, this.CodeMirror.state.doc.length, 100000);
   }
 
   /** ForceLint: Force the editor to do another round of linting. */
