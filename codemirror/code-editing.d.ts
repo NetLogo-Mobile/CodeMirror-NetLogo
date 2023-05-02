f85635aae0cdb4cb29c92b296dd7db1d35310310
CIVITAS-John <CIVITAS-John@users.noreply.github.com>
2023-05-02 23:35:31 +0000

    deploy: 04771e71d294cc68d8748402533aa6201d9a1726

--- codemirror/code-editing.d.ts
+++ codemirror/code-editing.d.ts
@@ -1,5 +1,10 @@
 import { EditorView } from 'codemirror';
-export declare const AppendGlobal: (view: EditorView, content: string, statement_type: string) => void;
-export declare const AddBreed: (view: EditorView, breed: string, plural: string, singular: string) => void;
-export declare const AddBreedVar: (view: EditorView, breed: string, var_name: string) => void;
-export declare const ReplaceProcedure: (view: EditorView, name: string, content: string) => void;
+import { SyntaxNode } from '@lezer/common';
+/** GalapagosEditing: Functions for editing code. */
+export declare class GalapagosEditing {
+    AppendGlobal(view: EditorView, content: string, statement_type: String): void;
+    AddBreed(view: EditorView, breed: string, plural: string, singular: string): void;
+    AddBreedVariable(view: EditorView, breed: string, varName: string): void;
+    ReplaceProcedure(view: EditorView, name: string, content: string): void;
+    AddTermToBracket(view: EditorView, content: string, node: SyntaxNode): void;
+}
