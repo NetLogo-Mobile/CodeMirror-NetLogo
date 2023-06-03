57ca8e0f096de980d6c70447bf88f24d82530396
CIVITAS-John <CIVITAS-John@users.noreply.github.com>
2023-06-03 02:36:56 +0000

    deploy: 2fe08039a7bca08fb4903b4886d461d6ea95ba7f

--- lang/specializer.d.ts
+++ lang/specializer.d.ts
@@ -2,5 +2,5 @@ declare const specializeReporter: (token: string) => any;
 declare const specializeSpecialReporter: (token: string) => any;
 declare const specializeCommand: (token: string) => any;
 declare const specializeSpecialCommand: (token: string) => any;
-export { specializeReporter, specializeSpecialReporter, specializeCommand, specializeSpecialCommand, };
+export { specializeReporter, specializeSpecialReporter, specializeCommand, specializeSpecialCommand };
 //# sourceMappingURL=specializer.d.ts.map
