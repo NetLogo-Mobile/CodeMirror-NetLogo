920ac275980ea6865ef52f82a3f1fdefc5b64669
CIVITAS-John <CIVITAS-John@users.noreply.github.com>
2023-07-02 18:15:14 +0000

    deploy: 4e43f676016f962bf365e5991af12d23af8c5129

--- editor.bundle.js
+++ editor.bundle.js
@@ -26854,8 +26854,29 @@ if(!String.prototype.matchAll) {
     }
     catch (error) { }
 
+    /** Global: Global object. */
+    const Global = typeof globalThis === 'undefined' ? window : globalThis;
     /** Log: Log to console if debug is enabled. */
-    const Log = !(globalThis !== null && globalThis !== void 0 ? globalThis : window).GalapagosSilent ? console.log : () => { };
+    const Log = Global.GalapagosSilent ? console.log : () => { };
+    /**
+     * String.prototype.trimStart() polyfill
+     * Adapted from polyfill.io
+     */
+    if (!String.prototype.trimStart)
+        String.prototype.trimStart = function () {
+            return this.replace(new RegExp('^' +
+                /[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+/
+                    .source, 'g'), '');
+        };
+    /**
+     * String.prototype.trimEnd() polyfill
+     * Adapted from polyfill.io
+     */
+    if (!String.prototype.trimEnd)
+        String.prototype.trimEnd = function () {
+            return this.replace(new RegExp(/[\x09\x0A\x0B\x0C\x0D\x20\xA0\u1680\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205F\u3000\u2028\u2029\uFEFF]+/
+                .source + '$', 'g'), '');
+        };
 
     /** GetCursorUntilMode: Get the cursor until the "mode" node. */
     function GetCursorUntilMode(State) {
--- utils/debug-utils.d.ts
+++ utils/debug-utils.d.ts
@@ -1,3 +1,5 @@
+/** Global: Global object. */
+export declare const Global: any;
 /** Log: Log to console if debug is enabled. */
 export declare const Log: {
     (...data: any[]): void;
