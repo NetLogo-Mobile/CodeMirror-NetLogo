c10b8ef2f48a91703d676e0885ae43c0e59b9e66
Acero Liang <aceroliang@Aceros-MacBook-Air.local>
2024-01-19 12:16:56 -0500

    widget is the right color

--- src/codemirror/cp-widget-extension.ts
+++ src/codemirror/cp-widget-extension.ts
@@ -7,24 +7,77 @@ import { syntaxTree } from '@codemirror/language';
 import { TreeCursor } from '@lezer/common';
 import { ColorPicker } from '@netlogo/netlogo-color-picker';
 
+
+/** Color helpers (temporary) --> where can we get netlogo colors to rgb? */
+/** netlogoColorToHex: Converts NetLogo color to its hex string. */
+var colorTimesTen: number;
+var baseIndex: number;
+var r, g, b: number;
+var step: number;
+const netlogoBaseColors: [number, number, number][] = [
+  [140, 140, 140], // gray       (5)
+  [215, 48, 39], // red       (15)
+  [241, 105, 19], // orange    (25)
+  [156, 109, 70], // brown     (35)
+  [237, 237, 47], // yellow    (45)
+  [87, 176, 58], // green     (55)
+  [42, 209, 57], // lime      (65)
+  [27, 158, 119], // turquoise (75)
+  [82, 196, 196], // cyan      (85)
+  [43, 140, 190], // sky       (95)
+  [50, 92, 168], // blue     (105)
+  [123, 78, 163], // violet   (115)
+  [166, 25, 105], // magenta  (125)
+  [224, 126, 149], // pink     (135)
+  [0, 0, 0], // black
+  [255, 255, 255], // white
+];
+let cachedNetlogoColors = (function () {
+  var k, results;
+  results = [];
+  for (colorTimesTen = k = 0; k <= 1400; colorTimesTen = ++k) {
+    baseIndex = Math.floor(colorTimesTen / 100);
+    [r, g, b] = netlogoBaseColors[baseIndex];
+    step = ((colorTimesTen % 100) - 50) / 50.48 + 0.012;
+    if (step < 0) {
+      r += Math.floor(r * step);
+      g += Math.floor(g * step);
+      b += Math.floor(b * step);
+    } else {
+      r += Math.floor((0xff - r) * step);
+      g += Math.floor((0xff - g) * step);
+      b += Math.floor((0xff - b) * step);
+    }
+    results.push([r, g, b]);
+  }
+  return results;
+})();
+let cached: number[][] = cachedNetlogoColors;
+
+function netlogoToRGB(netlogoColor: number): number[] {
+  let temp: number[] = cached[Math.floor(netlogoColor * 10)];
+  return [temp[0], temp[1], temp[2]];
+}
+
 /* ColorPickerWidget: Decoration Widget to open ColorPicker */
 class ColorPickerWidget extends WidgetType {
   private color: string;
   private length: number;
-  private textType: string;
-  constructor(widgetColor: string, length: number, textType: string) {
+  // true if the color associated with the widget is a netlogo color
+  private isNetLogoColor: boolean;
+  constructor(widgetColor: string, length: number, type: boolean) {
     super();
     this.color = widgetColor;
     this.length = length;
-    this.textType = textType;
+    this.isNetLogoColor = type;
   }
 
   getLength(): number {
     return this.length;
   }
 
-  getTextType(): string {
-    return this.textType;
+  isNetLogo() {
+    return this.isNetLogoColor;
   }
 
   toDOM(): HTMLElement {
@@ -52,49 +105,104 @@ class ColorPickerWidget extends WidgetType {
 
 /* ColorPicker Extension */
 
-function nodeIsColor(node: SyntaxNodeRef, view: EditorView, cursor: TreeCursor) {
-  const keywordIgnore = ['true', 'false', 'nobody', 'e', 'pi']; // constants to ignore
-  if (
-    node.name == 'Constant' &&
-    !keywordIgnore.includes(view.state.doc.sliceString(node.from, node.to).toLowerCase())
+/* to do:
+
+1) Make nodeIsColor return the color itself --> maybe return null if its not a color, returns the color itself as the color --> aggregate the color to be the same type --> rgba -- done 
+
+2) Make the node itself be the right color -- done 
+
+3) Make the colorPicker widget change the color based on the callback 
+
+4) -- color picker itself have the right color based on the starting color 
+*/
+
+
+
+/** getNodeColor: returns the color of a node given the node as a string. Returns empty string if not a color*/
+function getNodeColor(node: SyntaxNodeRef, view: EditorView, cursor: TreeCursor): string {
+  const ignore = ['true', 'false', 'nobody', 'e', 'pi']; //constants to ignore
+  let nodeVal = view.state.doc.sliceString(node.from, node.to);
+  if(
+    node.name == 'Constant' && 
+    !ignore.includes(nodeVal.toLowerCase()) 
   ) {
-    return true;
-  } else if (node.name == 'Numeric') {
-    //create a TreeCursor of the visible ranges.
+    return nodeVal;
+  } else if(node.name == 'Numeric') {
+    if(parseFloat(nodeVal) > 140 || parseFloat(nodeVal) < 0) {
+      return '';
+    }
+    // check if previous word is 'color'
     cursor.moveTo(node.from, 1);
-    // get previous word
     cursor.prev();
-    if (view.state.doc.sliceString(cursor.from, cursor.to).includes('color')) return true; // number preceded by "color"
+    if(view.state.doc.sliceString(cursor.from, cursor.to).includes('color')) return nodeVal;
   }
-  return false;
+  return '';
 }
+
+// function colorWidgets(view: EditorView, posToWidget: Map<number, ColorPickerWidget>): DecorationSet {
+//   let widgets: Range<Decoration>[] = [];
+//   posToWidget.clear(); // create new mappings on change in view
+//   for (let { from, to } of view.visibleRanges) {
+//     let cursor: TreeCursor = syntaxTree(view.state).cursor(); // create a cursor per change in view
+//     syntaxTree(view.state).iterate({
+//       from,
+//       to,
+//       enter: (node) => {
+//         if (nodeIsColor(node, view, cursor)) {
+//           let parseVal: string = view.state.doc.sliceString(node.from, node.to);
+//           let widgetColor: number;
+//           let textType: string;
+//           if (Number.isNaN(parseFloat(parseVal))) {
+//             textType = 'string';
+//           } else {
+//             widgetColor = parseFloat(parseVal);
+//             textType = 'number';
+//           }
+//           let cp_widget = new ColorPickerWidget('blue', parseVal.length, textType);
+//           let deco = Decoration.widget({
+//             widget: cp_widget,
+//             side: 1,
+//           });
+//           widgets.push(deco.range(node.to));
+//           posToWidget.set(node.to, cp_widget);
+//         }
+//       },
+//     });
+//   }
+//   return Decoration.set(widgets);
+// }
 
 function colorWidgets(view: EditorView, posToWidget: Map<number, ColorPickerWidget>): DecorationSet {
   let widgets: Range<Decoration>[] = [];
-  posToWidget.clear(); // create new mappings on change in view
+  posToWidget.clear();
+  let nodeVal: string;
   for (let { from, to } of view.visibleRanges) {
-    let cursor: TreeCursor = syntaxTree(view.state).cursor(); // create a cursor per change in view
+    let cursor: TreeCursor = syntaxTree(view.state).cursor();
     syntaxTree(view.state).iterate({
       from,
       to,
       enter: (node) => {
-        if (nodeIsColor(node, view, cursor)) {
-          let parseVal: string = view.state.doc.sliceString(node.from, node.to);
-          let widgetColor: number;
-          let textType: string;
-          if (Number.isNaN(parseFloat(parseVal))) {
-            textType = 'string';
-          } else {
-            widgetColor = parseFloat(parseVal);
-            textType = 'number';
+        if((nodeVal = getNodeColor(node, view, cursor)) != '') {
+          // determine the color type
+          let isNetLogo;
+          if(Number.isNaN(parseFloat(nodeVal))) {
+            isNetLogo = false;  // isNetLogo color 
+          }else {
+            isNetLogo = true;
           }
-          let cp_widget = new ColorPickerWidget('blue', parseVal.length, textType);
+          // translate to rgb if netlogoColor 
+          let color:string = nodeVal;
+          if(isNetLogo) {
+            let rgb = netlogoToRGB(parseFloat(nodeVal));
+            color = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
+          }
+          let cpWidget = new ColorPickerWidget(color, nodeVal.length, isNetLogo);
           let deco = Decoration.widget({
-            widget: cp_widget,
+            widget: cpWidget,
             side: 1,
           });
           widgets.push(deco.range(node.to));
-          posToWidget.set(node.to, cp_widget);
+          posToWidget.set(node.to, cpWidget);
         }
       },
     });
@@ -121,9 +229,8 @@ function initializeCP(view: EditorView, pos: number, widget: ColorPickerWidget) 
 
     let change;
     let length = widget.getLength();
-    let textType = widget.getTextType();
 
-    if (textType === 'number') {
+    if (widget.isNetLogo()) {
       change = { from: pos - length, to: pos, insert: selectedColor.toString() };
       view.dispatch({ changes: change });
     } else {
